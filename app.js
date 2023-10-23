3e2207a96d742f5dcb4108602fc5a6eb18414364
MangeshCG07 <120176420+MangeshCG07@users.noreply.github.com>
2023-10-23 16:33:50 +0530

    input output process done

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
-
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
+function clickHandler(){
+    outputDiv.innerText = "vfvbdfkvbkjvbdkfjvkjbi: " + txtInput.value;  
+ };
+
 
 btnTranslate.addEventListener("click",clickHandler)
 
-function clickHandler(){
-    console.log("clickkkkkked");
-    console.log("input", txtInput.value);
-}
